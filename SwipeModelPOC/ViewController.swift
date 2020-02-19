f9730628be4c016c61ce87e95028139dfaeb0180
Nutdanai Vankrua <nutdanai@odd.works>
2020-02-19 18:09:36 +0700

    Initial Commit

---
+++ SwipeModelPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeModelPOC
+//
+//  Created by Nutdanai Vankrua on 19/2/2563 BE.
+//  Copyright © 2563 AceQuery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
